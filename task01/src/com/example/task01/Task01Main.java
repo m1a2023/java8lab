097c493dbe2bee1c98dab76ba42ca03999a35354
m1a2023 <mag2017go@gmail.com>
2024-11-23 16:31:50 +0500

    done

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,20 +1,55 @@
 package com.example.task01;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.lang.ProcessBuilder;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
-        System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
+        System.out.println(extractSoundName(
+            new File("/home/mia/dev/csu/java/java8lab/task01/src/main/resources/3726.mp3"))
+        );
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+        
+        ProcessBuilder processBuilder = 
+            new ProcessBuilder(                                         
+                "ffprobe", "-v", "error", "-of", "flat", "-show_format", 
+                file.getAbsolutePath()                                   
+            );
+        
+        Process process = processBuilder.start();
+        
+        String generalOutput = getGeneralOutput(process.getInputStream());
+
+        return findTitle(generalOutput); 
+    }
+
+    private static String getGeneralOutput(InputStream inputStream) throws IOException{
+        ByteArrayOutputStream result = new ByteArrayOutputStream();
+
+        byte[] buffer = new byte[1024];
+
+        for (int length; (length = inputStream.read(buffer)) != -1; ) {
+            result.write(buffer, 0, length);
+        }
+
+        return result.toString("UTF-8");
+    }
+
+    private static String findTitle(String general) {
+        String find = "format.tags.title=\"";
+
+        int startIndexOfExpression = general.indexOf(find) + find.length();
+        int endIndexOfExpresiion = startIndexOfExpression;
+        
+        for (int i = startIndexOfExpression;
+            general.charAt(i) != '"'; i++, 
+            endIndexOfExpresiion = i);
+
+        return general.substring(startIndexOfExpression, endIndexOfExpresiion);
     }
 }
--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,36 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Task02Main {
+    
+    private static final List<Path> list = new ArrayList<Path>();
     public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
-        System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
-        */
-
+        listFiles(Paths.get("task02/src/main/resources/")).forEach((file) -> {
+            System.out.println(file);
+        });
     }
 
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
 
-        return null;
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(rootDir)) {
+            for (Path entry : stream) {
+                if (Files.isDirectory(entry)) {
+                    listFiles(entry); 
+                } else {
+                    list.add(entry);
+                }
+            }
+
+            stream.close();
+        }
+
+        return list;
     }
 }
